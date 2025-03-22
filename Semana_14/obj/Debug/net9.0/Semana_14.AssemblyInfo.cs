1593ad6ce8eeedf0490137cc3135381e4a363028
ljrodriguezr <lj.rodriguezr@uea.edu.ec>
2025-03-21 20:34:35 -0500

    tarea semana 14

--- Semana_14/obj/Debug/net9.0/Semana_14.AssemblyInfo.cs
+++ Semana_14/obj/Debug/net9.0/Semana_14.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Semana_14")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+466591fd4ec8d6a805b349fcb10dc15617bc97f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b883d8e0cfe283a7b87850c845dc8d25b12e5063")]
 [assembly: System.Reflection.AssemblyProductAttribute("Semana_14")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Semana_14")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
